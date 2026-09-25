3f87d32cbd467e1bf4559f2755aa80f73fcfddd2
gauravdongare288 <gauravdongare288@gmail.com>
2026-09-25 17:21:06 +0530

    Create Unit2_program2.cpp

---
+++ Unit2_program2.cpp
@@ -0,0 +1,25 @@
+/*Program (2) -> Write Write a program to make use of basic Input/Output functions
+                using different data types.
+  Solution(2) -> User Define Values */
+
+# include <stdio.h>
+int main()
+{
+   int rollnum;
+   float per;
+   char grade;
+
+   printf("\n Enter Student Roll Number: “):
+   scanf("%d", &rollnum)
+   printf(“In Enter Student Percentage: "):
+   scanf("%f', &per);
+   printf(“\n Enter Student Grade: “);
+   scanf(“ %c", &grade)
+
+   printf("\n---- Student Information ----In"):
+   printf("\n Roll Number is: %d", rollnum);
+   printf("\n Percentage is: %6f", per);
+   printf(“ln Grade ：%c", grade);
+
+   return O;
+}
