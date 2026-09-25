54509ba62749e8b6ec3351ddb38964bb383ade86
gauravdongare288 <gauravdongare288@gmail.com>
2026-09-25 19:55:44 +0530

    Create Unit1_program7.cpp

---
+++ Unit1_program7.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+
+{
+  int a, b,temp;
+  
+  a = 111;
+  b = 222;
+  printf("\n Before swapping:");
+  printf("\n a = %d, b = %d \n", a,b);
+
+  temp = a;
+  a = b;
+  b = temp;
+
+  printf("In After swapping:“);
+  printf("\n a =%d, b= %d \n", a, b);
+ 
+  return O;
+}
