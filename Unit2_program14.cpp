e3540f2a18c327138b2c522d0d727a7c706c5c7a
gauravdongare288 <gauravdongare288@gmail.com>
2026-09-25 21:00:25 +0530

    Create Unit2_program14.cpp

---
+++ Unit2_program14.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int num, i= 1;
+
+   printf("Enter a number: “);
+   scanf(“%d", &num);
+
+   printf("\n Multiplication Table of %d In", num);
+
+   while (i <= 10)
+  {
+    printf(“%dx%d=%d\n", num, i, num *i);
+    i**;
+   }
+   return 0;
+}
